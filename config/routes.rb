10f5b25a4f1acf341a85841a1668cb77eaccf355
S-higss <shonoji2001@gmail.com>
2024-07-18 11:55:32 +0000

    [Do]3.2.1 exercises1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
-  root "hello#index"
+  # root "hello#index"
+  root 'static_pages#home'
 end
