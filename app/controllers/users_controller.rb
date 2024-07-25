f278c3d108fc2c718e46b6c7610cc21938d05790
S-higss <shonoji2001@gmail.com>
2024-07-25 07:01:21 +0000

    [Add]user sign up method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # xx
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
 end
