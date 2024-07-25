f7f0fa4c5b2debc834fdf2ef61ac105249d8f10c
S-higss <shonoji2001@gmail.com>
2024-07-25 04:34:30 +0000

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
-  # before_save { self.email = email.downcase }
-  # before_save { self.email = self.email.downcase } <= is ok too.
+  # => before_save { self.email = email.downcase }
+  #    before_save { self.email = self.email.downcase } either is ok.
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
