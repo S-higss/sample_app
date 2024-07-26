317e111a081283ab4cd77f82b3061b27dd9467b9
S-higss <shonoji2001@gmail.com>
2024-07-26 01:18:35 +0000

    [Add]flash on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
       # -> is equal to -> redirect_to user_url(@user)
     else
