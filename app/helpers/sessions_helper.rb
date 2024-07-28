6b5b3af9420c7d16a7b53931f54ae26366327397
S-higss <shonoji2001@gmail.com>
2024-07-28 02:36:40 +0000

    [Add]login by permanent cookies to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,8 +13,14 @@ module SessionsHelper
 
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
