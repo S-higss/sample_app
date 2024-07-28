2c6bd4d0e8eca5d74b185982903c560baf787857
S-higss <shonoji2001@gmail.com>
2024-07-28 16:52:08 +0000

    [Add]edit action on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = user.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
