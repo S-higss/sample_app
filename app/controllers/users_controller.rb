cf316475022f2f96b245f7a1ff86b08a3920754a
S-higss <shonoji2001@gmail.com>
2024-07-29 04:30:41 +0000

    [Add]update action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,14 +19,24 @@ class UsersController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
-
+  
   def edit
     @user = User.find(params[:id])
   end
-
-  private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新成功した場合を扱う
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
     end
+  end
+  
+  private
+  def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
